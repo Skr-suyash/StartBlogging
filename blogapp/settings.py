0a49ec585217941a2c4cabd306fb91f768b073b8
= <=>
2020-05-22 23:24:50 +0530

    added django heroku

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from django.urls import reverse_lazy
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +139,5 @@ AUTHENTICATION_BACKENDS = (
     )
 
 LOGIN_URL = reverse_lazy('login')
+
+django_heroku.settings(locals())
