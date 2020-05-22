752561b57f336e9e4df88012c8fe62ce26f839ff
= <=>
2020-05-22 23:07:34 +0530

    Changed settings

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cs(#r201-%fq$)hpei58)(j$p%n&-4=t_igzr$drwen7^pz00r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
